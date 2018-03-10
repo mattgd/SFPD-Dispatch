0a4c7967b08eabfdeb4b82adf0df3996fa51ad0e
mattgd <hello@mattdzwonczyk.com>
2018-03-10 18:13:18 -0500

    Add average calls per hour of day metric.

--- dispatch/metrics/views.py
+++ dispatch/metrics/views.py
@@ -21,15 +21,27 @@ def get_metrics(request):
         "avg_response_time": {
             "groups": [],
             "data": []
+        },
+        "avg_calls_per_hour": {
+            "data": []
         }
     }
     calls = Call.objects.all()
     avg_resp_time_data = {}
+    
+    avg_calls_per_hour = {}
+    for i in range(0, 24):
+        avg_calls_per_hour[i] = 0
+    days = []
+
+    total_days = 0
 
     for call in calls:
+        # Collect average response time data
         if call.response_timestamp:
             response_time = call.response_timestamp - call.received_timestamp
             group = call.call_type_group if call.call_type_group else "Other"
+            #group = call.call_type if call.call_type else "Other"
 
             if group in avg_resp_time_data:
                 avg_resp_time_data[group]["avg_time"] += response_time
@@ -38,7 +50,7 @@ def get_metrics(request):
                 avg_resp_time_data[group] = {
                     "avg_time": response_time,
                     "total_calls": 1
-                } 
+                }
                 
             """
             data.append(
@@ -48,13 +60,23 @@ def get_metrics(request):
                 }
             )
             """
+        
+        if call.received_timestamp:
+            received_time = call.received_timestamp
+            hour = received_time.hour
+            day_key = received_time.strftime("%m/%d")
+
+            if day_key not in days:
+                total_days += 1
+                days.append(day_key)
+            
+            avg_calls_per_hour[hour] += 1
+            
 
     for group in avg_resp_time_data:
         avg_resp_time_data[group]["avg_time"] = (avg_resp_time_data[group]["avg_time"]
             / avg_resp_time_data[group]["total_calls"])
         data["avg_response_time"]["groups"].append(group)
-
-        #print(str(avg_resp_time_data[group]["avg_time"]))
 
         """
         data["avg_response_time"]["data"].append(
@@ -64,12 +86,20 @@ def get_metrics(request):
             }
         )
         """
-        print(str(avg_resp_time_data[group]["avg_time"]))
-        print(avg_resp_time_data[group]["avg_time"].seconds)
         seconds = avg_resp_time_data[group]["avg_time"].seconds
         data["avg_response_time"]["data"].append(
             round(seconds / 60, 2)
         )  
 
+    # Calculate average calls per hour
+    for hour in avg_calls_per_hour:
+        data["avg_calls_per_hour"]["data"].append(round(avg_calls_per_hour[hour]
+             / total_days, 2))
+
+    data["avg_calls_per_hour"]["labels"] = [
+        "12 AM", "1 AM", "2 AM", "3 AM", "4 AM", "5 AM", "6 AM", "7 AM", "8 AM",
+        "9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM",
+        "5 PM", "6 PM", "7 PM", "8 PM", "9 PM", "10 PM", "11 PM"
+    ]
 
     return JsonResponse(data, safe=False)
